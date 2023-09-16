57e36d7cafd6a5fb3b253f810c7cf1262028f31c
Maaike Sloot <msloot@bess-f3r5s5.clusters.42paris.fr>
2023-09-16 13:04:31 +0200

    update

--- main.c
+++ main.c
@@ -1,29 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush_01.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 10:20:43 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/16 11:02:31 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/16 13:04:04 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_strlen(char *str);
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
 
 int	error()
 {
 	ft_putstr("Error\n");
 	return (1);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
 }
 
 int	parse(char *input, int view[9][9], int *n)
@@ -49,7 +43,7 @@ int	parse(char *input, int view[9][9], int *n)
 	return (1);
 }
 
-void	2d_bzero(int grid[9][9], int n)
+void	init_grid(int grid[9][9], int n)
 {
 	int	x;
 	int	y;
@@ -77,7 +71,7 @@ int	main(int argc, char *argv[])
 		return (error());
 	if (parse(argv[1], view, &n) == 0)
 		return (error());
-	2d_bzero(grid, n);
+	init_grid(grid, n);
 	if (solve(grid, view, n) == 0)
 		return (error());
 	// print the grid
