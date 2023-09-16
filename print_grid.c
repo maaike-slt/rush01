9e9f4cc812c771e55ed13c1df7d8dc25d6161370
Maaike Sloot <msloot@bess-f2r1s16.clusters.42paris.fr>
2023-09-16 17:09:33 +0200

    update

--- print_grid.c
+++ print_grid.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   print_grid.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 10:20:43 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/16 17:09:11 by msloot           ###   ########.fr       */
+/*   Created: 2023/09/16 17:06:29 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/16 17:09:17 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str);
-void	ft_putstr(char *str);
 void	ft_putchar(char c);
-int	parse(char *input, int view[4][9], int *n);
 
-int	error()
-{
-	ft_putstr("Error\n");
-	return (1);
-}
-
-void	init_grid(int grid[9][9], int n)
+void	print_grid(int grid[9][9], int n)
 {
 	int	x;
 	int	y;
@@ -32,26 +23,12 @@ void	init_grid(int grid[9][9], int n)
 		y = 0;
 		while (y < n)
 		{
-			grid[x][y] = 0;
+			ft_putchar(grid[x][y] + '0');
+			if (y < n - 1)
+				ft_putchar(' ');
 			y++;
 		}
+		ft_putchar('\n');
 		x++;
 	}
-}
-
-int	main(int argc, char *argv[])
-{
-	int	grid[9][9];
-	int	view[4][9];
-	int	n;
-
-	if (argc != 2)
-		return (error());
-	if (parse(argv[1], view, &n) == 0)
-		return (error());
-	init_grid(grid, n);
-	if (solve(grid, view, n) == 0)
-		return (error());
-	// print the grid
-	return (0);
 }
