902dee91139da512cd298e598b7bbd8732caa037
Maaike Sloot <msloot@bess-f2r1s16.clusters.42paris.fr>
2023-09-16 17:55:26 +0200

    changes

--- main.c
+++ main.c
@@ -6,37 +6,18 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 10:20:43 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/16 17:09:11 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/16 17:35:52 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str);
 void	ft_putstr(char *str);
-void	ft_putchar(char c);
 int	parse(char *input, int view[4][9], int *n);
+void	init_grid(int grid[9][9], int n);
 
 int	error()
 {
 	ft_putstr("Error\n");
 	return (1);
-}
-
-void	init_grid(int grid[9][9], int n)
-{
-	int	x;
-	int	y;
-
-	x = 0;
-	while (x < n)
-	{
-		y = 0;
-		while (y < n)
-		{
-			grid[x][y] = 0;
-			y++;
-		}
-		x++;
-	}
 }
 
 int	main(int argc, char *argv[])
